55bd2269eab871e224b45f01686f9c6ea2716e23
Raphaelli Bellini <rafael.bellini@hotmail.com>
2019-04-19 18:07:29 -0300

    Go to the search page by clicking on the FAB

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
 
 class Button extends Component {
     render() {
         return (
             <div className="open-search">
-                <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
+                <Link to='/search'>Add a book</Link>
             </div>
         )
     }
