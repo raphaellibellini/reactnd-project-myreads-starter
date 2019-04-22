e9a9ec56ece5fb407efc141ef76661e10309e368
Raphaelli Bellini <rafael.bellini@hotmail.com>
2019-04-22 15:43:00 -0300

    Update the project structure

--- src/App.js
+++ src/App.js
@@ -2,13 +2,41 @@ import React from 'react'
 import './App.css'
 import Search from './views/Search'
 import Main from './views/Main'
-import {Route} from 'react-router-dom' 
+import {Route} from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
 
 class BooksApp extends React.Component {
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll()
+      .then((books) => {
+        console.log(books)
+        this.setState(() => ({
+          books
+        }))
+      })
+  }
+
+  changeShelf = (updatedBook, shelf) => {
+    BooksAPI.update(updatedBook, shelf)
+        .then(resp => {
+            console.log(resp)
+            updatedBook.shelf = shelf;
+            this.setState(prevState => ({
+                books: prevState.books.filter(book => updatedBook.id !== book.id).concat([updatedBook])
+            }))
+        })
+  }
+
   render() {
     return (
       <div className="app">
-        <Route exact path='/' component={Main} />
+        <Route exact path='/' render={() => (
+          <Main books={this.state.books} changeShelf={this.changeShelf} />
+        )} />
         <Route exact path='/search' component={Search} />
       </div>
     )
--- src/views/Main.js
+++ src/views/Main.js
@@ -1,44 +1,20 @@
 import React, {Component} from 'react'
 import Shelf from '../components/Shelf'
 import Button from '../components/Button'
-import * as BooksAPI from '../BooksAPI'
 
 class Main extends Component {
-    state = {
-        books: []
-    }
-
-    componentDidMount() {
-        BooksAPI.getAll()
-          .then((books) => {
-            console.log(books)
-            this.setState(() => ({
-              books
-            }))
-          })
-    }
-
-    changeShelf = (updatedBook, shelf) => {
-        BooksAPI.update(updatedBook, shelf)
-            .then(resp => {
-                console.log(resp)
-                updatedBook.shelf = shelf;
-                this.setState(prevState => ({
-                    books: prevState.books.filter(book => updatedBook.id !== book.id).concat([updatedBook])
-                }))
-            })
-    }
-
     render(){
+        const {books, changeShelf} = this.props
+
         return(
             <div className="list-books">
                 <div className="list-books-title">
                     <h1>MyReads</h1>
                 </div>
                 <div className="list-books-content">
-                    <Shelf title='Currently Reading' booksOnTheShelf={this.state.books.filter(book => book.shelf === "currentlyReading")} changeShelf={this.changeShelf} />
-                    <Shelf title='Want to Read' booksOnTheShelf={this.state.books.filter(book => book.shelf === "wantToRead")} changeShelf={this.changeShelf} />
-                    <Shelf title='Read' booksOnTheShelf={this.state.books.filter(book => book.shelf === "read")} changeShelf={this.changeShelf} />
+                    <Shelf title='Currently Reading' booksOnTheShelf={books.filter(book => book.shelf === "currentlyReading")} changeShelf={changeShelf} />
+                    <Shelf title='Want to Read' booksOnTheShelf={books.filter(book => book.shelf === "wantToRead")} changeShelf={changeShelf} />
+                    <Shelf title='Read' booksOnTheShelf={books.filter(book => book.shelf === "read")} changeShelf={changeShelf} />
                 </div>
                 <Button />
             </div>
