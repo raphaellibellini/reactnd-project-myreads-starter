a68e4c26512c4e0bf997775bae10226a5cf118c1
Raphaelli Bellini <rafael.bellini@hotmail.com>
2019-04-24 03:39:33 -0300

    Show the names of the authors separated by commas

--- src/components/Book.js
+++ src/components/Book.js
@@ -18,7 +18,7 @@ class Book extends Component {
                         </div>
                     </div>
                     <div className="book-title">{this.props.book.title}</div>
-                    <div className="book-authors">{this.props.book.authors}</div>
+                    <div className="book-authors">{this.props.book.authors ? this.props.book.authors.join(', ') : ''}</div>
                 </div>
             </li>
         )
